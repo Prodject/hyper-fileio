6463638856e3257bdb7951da2b4c86b8eda85cad
João Alberto <joaoalberto.contato@gmail.com>
2017-01-13 01:39:32 -0200

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    path: './',
+    filename: 'index.js',
+    libraryTarget: 'commonjs'
+  },
+  plugins: [ new webpack.DefinePlugin({ 'global.GENTLY': false }) ],
+  externals: [ nodeExternals() ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
